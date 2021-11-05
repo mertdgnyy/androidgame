8bf0d7fc036fbc7313c0b4feb80718373ddcd84e
Mertt Doğanay <MERTLOL3@outlook.com>
2021-11-05 16:42:19 -0400

    onClick event added to the play button, if user clicks the imagebutton, Play Button Clicked message appears in logcat, new class GameStart created

---
+++ app/src/main/java/com/example/androidgame/GameStart.java
@@ -0,0 +1,14 @@
+package com.example.androidgame;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
